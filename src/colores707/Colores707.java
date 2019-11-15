d4e408967a90c4114ffbfa8b3af09f7b2aa81987
Your-Full-Name <your-email-address>
2019-11-14 21:40:09 -0300

    Cambio de colores favoritos

--- src/colores707/Colores707.java
+++ src/colores707/Colores707.java
@@ -5,9 +5,9 @@ public class Colores707 {
     
     public static void main(String[] args) {
         String color1, color2, color3;
-        color1= "AZUL";
+        color1= "ROSA";
         color2= "ROJO";
-        color3= "VERDE";
+        color3= "NARANJA";
         System.out.printf("%s es mi primer color favorito\n", color1);
         System.out.printf("%s es mi segundo color favorito\n", color2);
         System.out.printf("%s es mi tercer color favorito\n", color3);
