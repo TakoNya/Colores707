e0fb45cc9ccc85ca590c7b241d4c00918fa381b3
b2 <b2@Ter72-W10.cen.local>
2019-11-19 20:27:08 -0300

    se amplia la lista cuatro colores

--- src/colores707/Colores707.java
+++ src/colores707/Colores707.java
@@ -4,13 +4,15 @@ public class Colores707 {
 
     
     public static void main(String[] args) {
-        String color1, color2, color3;
-        color1= "ROSA";
+        String color1, color2, color3, color4;
+        color1= "AZUL";
         color2= "ROJO";
-        color3= "NARANJA";
+        color3= "PURPURA";
+        color4= "MAGENTA";
         System.out.printf("%s es mi primer color favorito\n", color1);
-        System.out.printf("%s es mi segundo color favorito\n", color2);
+        System.out.printf("%s no es mi color principal pero tambien me gusta\n", color2);
         System.out.printf("%s es mi tercer color favorito\n", color3);
+        System.out.printf("%s es mi ultimo color favorito\n", color4);
         
     }
     
